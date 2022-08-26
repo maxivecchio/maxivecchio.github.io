f276e2300df45c8711fe6e89df0c654eb8fdc804
max <escuela@davinci.edu.ar>
2022-08-26 12:43:28 -0300

    Mejorando codigo

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,31 @@
-document.getElementById("copy").onclick = function copy(){ 
-    alert("hola")
+
+var btnEncriptar = document.getElementById("encriptar")
+var btnDesencriptar = document.getElementById("desencriptar")
+var btnCopiar = document.getElementById("copiar")
+var input = document.getElementById("ingresar");
+
+function encriptar() {
+    let textoEncriptado = "";
+    texto.toLowerCase() = input.value
+    for (let i = 0; i < texto.length; i++) {
+        if (texto[i] == "a") {
+            texto[i] = "jsdkhf"
+        }
+        console.log(texto[i])
+        texto += texto[i]
+    }
 }
+
+function desencriptar() {
+    alert("Desencriptar " + input.value);
+}
+
+function copiar() {
+    alert("Copiar " + input.value);
+}
+
+
+
+btnEncriptar.onclick = encriptar;
+btnDesencriptar.onclick = desencriptar;
+btnCopiar.onclick = copiar;
