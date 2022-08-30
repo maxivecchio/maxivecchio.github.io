7a985743237d780ddf5dac667a03245c8621acfc
Maxi Vecchio <maximilianovecchio@gmail.com>
2022-08-29 22:48:41 -0300

    Boton Encriptar y Desencriptar funcionando

--- scripts/script.js
+++ scripts/script.js
@@ -3,15 +3,16 @@ let btnDesencriptar = document.getElementById("desencriptar");
 let btnCopiar = document.getElementById("copiar");
 let input = document.getElementById("ingresar");
 let inputResultado = document.getElementById("resultado");
-let encriptlist = ["mcbvnbc", "zvcxbczx", "qwtyqw", "ñlñlñl", "kfgjfgkhj"];
+let letrasBase = ["a", "e", "i", "o", "u"];
+let encriptlist = ["ai", "enter", "imes", "ober", "ufat"];
 
 function encriptar() {
 
     let textoEncriptado = "";
     let texto = input.value;
     texto = texto.toLowerCase();
-    console.log(texto);
-    console.log(texto.length);
+/*     console.log(texto);
+    console.log(texto.length); */
 
     for (let i = 0; i < texto.length; i++) {
         if (texto.charAt(i) == "a") {
@@ -28,13 +29,13 @@ function encriptar() {
             textoEncriptado += texto.charAt(i);
         }
 
-        console.log(texto.charAt(i));
-
+/*         console.log(texto.charAt(i));
+ */
     }
 
-    console.log(texto);
-    resultadoMostrar(textoEncriptado);
-
+/*     console.log(texto);
+ */    resultadoMostrar(textoEncriptado);
+    limpiarInput();
 }
 
 function desencriptar() {
@@ -43,27 +44,50 @@ function desencriptar() {
     let textoDesencriptado = "";
     texto = texto.toLowerCase();
 
-    console.log("Desencriptar " + input.value);
+/*     console.log("Desencriptar " + input.value);
+    console.log("-------------------------------")
+ */
+    /* let letrasBase = ["a","e","i","o","u"]
+    let encriptlist = ["ai", "enter", "imes", "ober", "ufat"];
+     */
+    /* hola -> hoberlai
+    recorrer hoberlai
+    */
 
-    // recorre todos los items de encriptlist
-    // por cada caracter ingresado en input.value
+    // i = indice letra del texto a desencriptar
+    // ie = indice de letra de encriptlist
+    // Cada carecter de encriptlist encriptlist[ie].charAt(i3)
+    // longitud encriptlist[ie].length
+
     for (let i = 0; i < texto.length; i++) {
-        for (let i2 = 0; i2 < encriptlist.length; i2++) {
-            for (let i3 = 0; i3 < encriptlist[i2].length; i3++) {
-                console.log(encriptlist[i2].charAt(i3));
-                /*
-                if (texto.charAt(i) == encriptlist[i2].charAt(i3)) {
-                    console.log("prueba")
-                    console.log(texto.charAt(i))
-                    console.log(encriptlist[i2].charAt(i3))
+        //console.log(texto[i]);
+        let verif = false;
+        for (let ie = 0; ie < encriptlist.length; ie++) {
+            let contador = 0;
+            //console.log(encriptlist[i2].charAt(0))
+            if (encriptlist[ie].charAt(0) == texto[i]) {
+/*                     console.log(encriptlist[ie].length)
+ */                    for (let i3 = 0; i3 < encriptlist[ie].length; i3++) {
+/*                         console.log(encriptlist[ie].charAt(i3))
+ */                        if (encriptlist[ie].charAt(i3) == texto[i + i3]) {
+                        contador++
+                    }
+                    if (contador == encriptlist[ie].length) {
+                        textoDesencriptado += letrasBase[ie]
+                        i = i + encriptlist[ie].length - 1
+                        verif = true;
+                    }
                 }
-                */
             }
 
+        }
+        if (verif == false) {
+            textoDesencriptado += texto[i]
         }
     }
 
     resultadoMostrar(textoDesencriptado);
+    limpiarInput();
 
 }
 
@@ -71,14 +95,17 @@ function copiar() {
     /*  inputResultado.select(); inputResultado.setSelectionRange(0, 99999); */
 
     navigator.clipboard.writeText(inputResultado.value);
-    console.log(inputResultado.value);
-
+/*     console.log(inputResultado.value);
+ */
 }
 
 function resultadoMostrar(txt) {
     inputResultado.value = txt;
 }
 
+function limpiarInput() {
+    input.value = "";
+}
 
 btnEncriptar.onclick = encriptar;
 btnDesencriptar.onclick = desencriptar;
